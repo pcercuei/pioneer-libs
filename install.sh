8c7a5e5bcff0d9e93f084ee998bf78cd9a6cdc0f
Paul Cercueil <paul@crapouillou.net>
2019-01-13 10:46:29 -0300

    Rename libraries to fit Windows naming scheme

--- install.sh
+++ install.sh
@@ -23,3 +23,24 @@ for i in /var/cache/pacman/pkg/*.tar.xz; do tar xJf $i; rm $i; done
 
 cp /c/projects/pioneer-libs/SDL_config_windows.h /c/build/mingw64/include/SDL2/SDL_config.h
 cp /mingw64/x86_64-w64-mingw32/lib/libmingw32.a /c/build/mingw64/lib/mingw32.lib
+
+# Rename dynamic libraries from libfoo.dll.a pattern to foo.lib
+cd /c/build/mingw64/lib
+for i in *.dll.a
+do
+	NEW_NAME=$(echo $i |sed 's/lib\(.*\).dll.a/\1.lib/' -)
+	mv $i $NEW_NAME
+done
+
+# Rename static libraries from libfoo.a pattern to foo.lib
+for i in *.a
+do
+	NEW_NAME=$(echo $i |sed 's/lib\(.*\).a/\1.lib/' -)
+	if [ -f $NEW_NAME ]
+	then
+		# Use the dynamic library if it exists
+		rm $i
+	else
+		mv $i $NEW_NAME
+	fi
+done
